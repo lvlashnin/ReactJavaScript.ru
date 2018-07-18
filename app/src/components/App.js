0f0814aaf69889b17239a59a6ef07fcf7d15bb7d
lvlashnin <lvlashnin@users.noreply.github.com>
2018-07-18 20:05:59 +0300

    add sselect component

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import {articles} from '../fixtures';
 // import {render} from 'react-dom'
-// import ArticleList from './components/ArticleList'
 // import './App.css';
 import ArticleList from './ArticleList'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
 
 class App extends Component {
+  state = {
+    selection: null,
+
+  }
   render() {
+      const options = articles.map(article => ({  
+      label: article.title,
+      value: article.id    
+    }))
+
     return (<div>
+    <Select options={options} value={this.state.selection} onChange = {this.changeSelection} multi/>
       <ArticleList articles={articles}/>
             </div>
     )
   }
+  changeSelection = selection => this.setState({selection})
 }
 
 export default App;
